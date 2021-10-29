2b140962071a97b3aba4b85ca7447029ef6858b2
Shariful Islam <sharifulmamun7@gmail.com>
2021-10-29 15:35:46 +0600

    error page added

--- src/pages/NotFound/Notfound.js
+++ src/pages/NotFound/Notfound.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import errorImg from "../../images/404/error.png";
 
 const Notfound = () => {
   return (
-    <div>
-      <h1>this is 404 </h1>
+    <div className="mx-auto text-center my-4">
+      <img src={errorImg} alt="" />
+      <br />
+      <Link to="/home">
+        {" "}
+        <button className="btn btn-secondary my-5">Go Back Home</button>
+      </Link>
     </div>
   );
 };
