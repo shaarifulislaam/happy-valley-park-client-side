b4ac1123e97b49e15a574d1e6c62cf644da154c1
Shariful Islam <sharifulmamun7@gmail.com>
2021-10-31 17:27:49 +0600

    spinner added in service

--- src/pages/home/Services/Services.js
+++ src/pages/home/Services/Services.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { Row } from "react-bootstrap";
+import { Row, Spinner } from "react-bootstrap";
 import Service from "../Service/Service";
 
 const Services = () => {
   const [services, setServices] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch("https://still-ridge-26061.herokuapp.com/services/")
       .then((res) => res.json())
-      .then((data) => setServices(data));
+      .then((data) => {
+        setServices(data);
+        setLoading(false);
+      });
   }, []);
   return (
     <div id="services">
@@ -19,9 +22,13 @@ const Services = () => {
         xl={3}
         className="g-3 container-lg  mx-lg-auto mx-md-auto mx-0"
       >
-        {services.map((service) => (
-          <Service key={service._id} service={service}></Service>
-        ))}
+        {loading ? (
+          <Spinner className="text-center mx-auto" animation="border" />
+        ) : (
+          services.map((service) => (
+            <Service key={service._id} service={service}></Service>
+          ))
+        )}
       </Row>
     </div>
   );
